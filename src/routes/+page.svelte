335eeea432e40a0589f1724548d0263cec1ef293
jbouyer <100257222+jbouyer@users.noreply.github.com>
2024-12-26 01:11:48 -0800

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,5 +24,4 @@
         font-style: italic;
     }
 </style>
-<a href="/brucetest20">Check out the new page</a>
-<br/><a href="/brucetest3">Check out the new page</a>
+<br/><a href="/brucetest1">Check out the new page</a>
